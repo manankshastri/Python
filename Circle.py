53746f0773b0b03b55dbbeb1a80384d3831c70b8
manankshastri <manank.shastri@gmail.com>
2017-11-28 12:24:21 -0500

    programs

---
+++ Circle.py
@@ -0,0 +1,24 @@
+import turtle
+import random
+
+def draw_square():
+    # random colors
+    col = ["red","white","blue","yellow","green"]
+    window = turtle.Screen()
+    window.bgcolor("black")
+
+    pp = turtle.Turtle()
+    pp.shape("arrow")
+    pp.color("white")
+    pp.speed(0)
+
+    for k in range(360):            #loop to turn the square
+        for i in range(4):          #to make a simple square
+            pp.forward(100)         #forward step
+            pp.right(90)            #to make a turn
+            pp.color(random.choice(col))        #changing the color
+        pp.right(1)                 #the degrees the square will turn
+
+    window.exitonclick()
+
+draw_square()
