b38fb068aba1ef1dfdb827c451b2ab543b62f7c8
Manank Shastri <32018919+manankshastri@users.noreply.github.com>
2017-11-28 12:28:34 -0500

    updated comments

--- Circle.py
+++ Circle.py
@@ -1,3 +1,4 @@
+# a circle is drawn using squares
 import turtle
 import random
 
@@ -5,6 +6,8 @@ def draw_square():
     # random colors
     col = ["red","white","blue","yellow","green"]
     window = turtle.Screen()
+    
+    # setting the window background to black
     window.bgcolor("black")
 
     pp = turtle.Turtle()
@@ -16,7 +19,7 @@ def draw_square():
         for i in range(4):          #to make a simple square
             pp.forward(100)         #forward step
             pp.right(90)            #to make a turn
-            pp.color(random.choice(col))        #changing the color
+            pp.color(random.choice(col))        #changing the color of every line drawn
         pp.right(1)                 #the degrees the square will turn
 
     window.exitonclick()
