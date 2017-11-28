64d494017c1f011b75401dbfedda2edf2c655b03
Manank Shastri <32018919+manankshastri@users.noreply.github.com>
2017-11-28 12:25:42 -0500

    Update browserOpen.py

--- browserOpen.py
+++ browserOpen.py
@@ -6,6 +6,7 @@ print("The program started on "+ time.ctime())
 for i in range(3):
     #time delay
     time.sleep(5)
+    
     # after the time, this website will open
     # the website will open after every 5 seconds, 3 times
     webbrowser.open("https://www.youtube.com/watch?v=8f7wj_RcqYk")
