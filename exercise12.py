626455cf1dbbc35d8a1d659bdc5f6a7745551557
Manank Shastri <32018919+manankshastri@users.noreply.github.com>
2017-12-04 19:14:42 -0500

    comments added

--- exercise12.py
+++ exercise12.py
@@ -1,11 +1,12 @@
+#function prints the fibonacci series
 def fib(n):
     f0 = 0
     f1 = 1
     print("Fibonacci Series: ")
     for i in range(n):
-        print(f0, end = " ")
+        print(f0, end = " ")            #end = " " - to print all the elements in a single line
         fn = f0 +f1
         f0 = f1
         f1 = fn
 
-fib(6)
+fib(6)                  #prints first six numbers 
